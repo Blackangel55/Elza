f433d1c0d70280c83a91799edcc4eefd93241f54
Blackangel <131115748+Blackangel55@users.noreply.github.com>
2026-06-12 17:33:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1905,7 +1905,7 @@ async def auto_filter(client, msg, spoll=False):
             m=await message.reply_text(f'<b>Wᴀɪᴛ {message.from_user.mention} Sᴇᴀʀᴄʜɪɴɢ Yᴏᴜʀ Qᴜᴇʀʏ :<i>{search}...</i></b>', reply_to_message_id=message.id)
             find = search.split(" ")
             search = ""
-            removes = ["in","upload", "series", "full", "horror", "thriller", "mystery", "print", "file"]
+            removes = ["upload", "series", "horror", "thriller", "print", "file","movie","movies"]
             for x in find:
                 if x in removes:
                     continue
